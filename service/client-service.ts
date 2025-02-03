ce30a9178cc21bd0627751365cde0db3d2b3eb6e
dilshan7542 <dilshan_k@epiclanka.net>
2025-02-03 16:19:14 +0530

    commit  5

--- service/client-service.ts
+++ service/client-service.ts
@@ -1,23 +1,26 @@
 import apiClient, {AppResponse} from "@/service/api-client";
-export interface ReadyToCheckout{
-  "fromAccountList":string[],
+
+export interface ReadyToCheckout {
+  "fromAccountList": string[],
   "toAccount": string,
   "toAccountName": string
 }
-export interface ProcessPaymentReq{
+
+export interface ProcessPaymentReq {
   'nic': string,
   'amount': number,
   'transactionRef': string,
 }
-export const readyToCheckoutApi=async (nic:string)=>{
-  alert("NIC : "+nic)
-  const response = await apiClient.post<AppResponse<ReadyToCheckout>>("/third-party/user/account-details",{nic:nic});
+
+export const readyToCheckoutApi = async (nic: string) => {
+  alert("NIC : " + nic)
+  const response = await apiClient.post<AppResponse<ReadyToCheckout>>("/third-party/user/account-details", {nic: nic});
   console.log(response);
   return response.data;
 }
-export const processPaymentApi=async (req:ProcessPaymentReq)=>{
-  console.log("Request Payment ",req)
-  const response = await apiClient.post<AppResponse<{webUrl:string}>>("/third-party/user/pay-lottery",req);
-  console.log("Response Process Payment ",response.data);
+export const processPaymentApi = async (req: ProcessPaymentReq) => {
+  console.log("Request Payment ", req)
+  const response = await apiClient.post<AppResponse<{ webUrl: string }>>("/third-party/user/pay-lottery", req);
+  console.log("Response Process Payment ", response.data);
   return response.data;
 }
